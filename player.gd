fe325b9a4ccd385b027c0a601d755fe30d7a5d94
Thibault Jan Beyer <thibault.beyer@gmail.com>
2026-09-05 00:59:33 +0700

    fix events

--- player.gd
+++ player.gd
@@ -37,10 +37,7 @@ func _process(delta):
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = false
 		# See the note below about the following boolean assignment.
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+		$AnimatedSprite2D.flip_h = velocity.x > 0
 
 
 func _on_body_entered(_body):
