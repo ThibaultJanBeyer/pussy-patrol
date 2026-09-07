780687ea0a798ba53f18a7f01828847f569896b0
Thibault Jan Beyer <thibault.beyer@gmail.com>
2026-09-07 14:15:37 +0700

    export game

--- exports/html/game.service.worker.js
+++ exports/html/game.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788746551|4458851';
+const CACHE_VERSION = '1788765325|19094157980';
 /** @type {string} */
 const CACHE_PREFIX = 'Pussy Patrol-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
